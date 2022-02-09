52f38e1f6093a0a5f6f48a8d6738b96b91ab8046
kenb9027 <kenb9027@gmail.com>
2022-02-09 16:13:09 +0100

    added Timer component and actions

---
+++ src/components/Timer.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+
+const Timer = ({route , navigation}) => {
+
+
+    const [name, setName] = useState("")
+    const [time, setTime] = useState(0)
+    const [isRunning , setIsRunning] = useState(false)
+  }
