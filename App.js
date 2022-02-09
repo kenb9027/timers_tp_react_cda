842995a0ee4b388dca37ec6d69f0bfe1c8847988
kenb9027 <kenb9027@gmail.com>
2022-02-09 16:19:18 +0100

    added Timer reducers

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>TP Timer </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <Text>TP Timer , go on !</Text>
+            </View>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
